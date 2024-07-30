8778d7136fd9e87199873aadd00abc6439b4e16f
Ressureição Macuácua <ressureicaomacuacua27@email.com>
2024-07-30 11:31:24 +0200

    segunda versao

--- src/main/java/com/projeto/sistema1/controle/FuncionarioControle.java
+++ src/main/java/com/projeto/sistema1/controle/FuncionarioControle.java
@@ -67,6 +67,74 @@
 //	
 //
 //}
+//package com.projeto.sistema1.controle;
+//
+//import java.util.Optional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//
+//import com.projeto.sistema1.modelos.Funcionario;
+//import com.projeto.sistema1.repositorios.FuncionarioRepositorio;
+//
+//@Controller
+//public class FuncionarioControle {
+//
+//    @Autowired
+//    private FuncionarioRepositorio funcionarioRepositorio;
+//
+//    @GetMapping("/cadastrarFuncionarioo")
+//    public String cadastrar(Model model) {
+//        model.addAttribute("funcionario", new Funcionario());
+//        return "administrativo/funcionarioss/cadastro";
+//    }
+//
+//    @GetMapping("/listarFuncionarioo")
+//    public String listar(Model model) {
+//        model.addAttribute("listaFuncionarios", funcionarioRepositorio.findAll());
+//        return "administrativo/funcionarioss/lista";
+//    }
+//
+//    @PostMapping("/salvarFuncionario")
+//    public String salvar(Funcionario funcionario, BindingResult result, RedirectAttributes redirectAttributes) {
+//        if (result.hasErrors()) {
+//            return "administrativo/funcionarioss/cadastro";
+//        }
+//
+//        // Encriptar senha (se necessário)
+//        // funcionario.setSenha(new BCryptPasswordEncoder().encode(funcionario.getSenha()));
+//
+//        funcionarioRepositorio.save(funcionario);
+//        redirectAttributes.addFlashAttribute("message", "Funcionário '" + funcionario.getNome() + "' salvo com sucesso!");
+//        return "redirect:/cadastrarFuncionarioo";
+//    }
+//
+//    @GetMapping("/editarFuncionario/{id}")
+//    public String editar(@PathVariable("id") Long id, Model model) {
+//        Optional<Funcionario> funcionario = funcionarioRepositorio.findById(id);
+//        if (funcionario.isPresent()) {
+//            model.addAttribute("funcionario", funcionario.get());
+//            return "administrativo/funcionarioss/cadastro";
+//        } else {
+//            return "redirect:/listarFuncionarioo";
+//        }
+//    }
+//
+//    @GetMapping("/removerFuncionario/{id}")
+//    public String remover(@PathVariable("id") Long id) {
+//        Optional<Funcionario> funcionario = funcionarioRepositorio.findById(id);
+//        if (funcionario.isPresent()) {
+//            funcionarioRepositorio.delete(funcionario.get());
+//        }
+//        return "redirect:/listarFuncionarioo";
+//    }
+//}
 package com.projeto.sistema1.controle;
 
 import java.util.Optional;
@@ -78,7 +146,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.projeto.sistema1.modelos.Funcionario;
 import com.projeto.sistema1.repositorios.FuncionarioRepositorio;
@@ -90,48 +158,51 @@ public class FuncionarioControle {
     private FuncionarioRepositorio funcionarioRepositorio;
 
     @GetMapping("/cadastrarFuncionarioo")
-    public String cadastrar(Model model) {
-        model.addAttribute("funcionario", new Funcionario());
-        return "administrativo/funcionarioss/cadastro";
+    public ModelAndView cadastrar(Funcionario funcionario) {
+        ModelAndView mv = new ModelAndView("administrativo/funcionarioss/cadastro");
+        mv.addObject("funcionario", funcionario);
+        return mv;
     }
 
     @GetMapping("/listarFuncionarioo")
-    public String listar(Model model) {
-        model.addAttribute("listaFuncionarios", funcionarioRepositorio.findAll());
-        return "administrativo/funcionarioss/lista";
+    public ModelAndView listar() {
+        ModelAndView mv = new ModelAndView("administrativo/funcionarioss/lista");
+        mv.addObject("listaFuncionarios", funcionarioRepositorio.findAll());
+        return mv;
     }
 
     @PostMapping("/salvarFuncionario")
-    public String salvar(Funcionario funcionario, BindingResult result, RedirectAttributes redirectAttributes) {
+    public ModelAndView salvar(Funcionario funcionario, BindingResult result, Model model) {
         if (result.hasErrors()) {
-            return "administrativo/funcionarioss/cadastro";
+            return cadastrar(funcionario);
         }
 
         // Encriptar senha (se necessário)
         // funcionario.setSenha(new BCryptPasswordEncoder().encode(funcionario.getSenha()));
 
-        funcionarioRepositorio.save(funcionario);
-        redirectAttributes.addFlashAttribute("message", "Funcionário '" + funcionario.getNome() + "' salvo com sucesso!");
-        return "redirect:/listarFuncionarioo";
+        funcionarioRepositorio.saveAndFlush(funcionario);
+        String mensagem = "Funcionário '" + funcionario.getNome() + "' salvo com sucesso!";
+        model.addAttribute("message", mensagem);
+        return cadastrar(new Funcionario());
     }
 
     @GetMapping("/editarFuncionario/{id}")
-    public String editar(@PathVariable("id") Long id, Model model) {
+    public ModelAndView editar(@PathVariable("id") Long id) {
         Optional<Funcionario> funcionario = funcionarioRepositorio.findById(id);
         if (funcionario.isPresent()) {
-            model.addAttribute("funcionario", funcionario.get());
-            return "administrativo/funcionarioss/cadastro";
+            return cadastrar(funcionario.get());
         } else {
-            return "redirect:/listarFuncionarioo";
+            // Redirecione para a lista se o funcionário não for encontrado
+            return listar();
         }
     }
 
     @GetMapping("/removerFuncionario/{id}")
-    public String remover(@PathVariable("id") Long id) {
+    public ModelAndView remover(@PathVariable("id") Long id) {
         Optional<Funcionario> funcionario = funcionarioRepositorio.findById(id);
         if (funcionario.isPresent()) {
             funcionarioRepositorio.delete(funcionario.get());
         }
-        return "redirect:/listarFuncionarioo";
+        return listar();
     }
 }
